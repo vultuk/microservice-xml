192e7334ccd6c258592cd43eca0210b3e419c8de
Simon Skinner <simon@vult.uk>
2021-04-19 11:23:08 +0100

    Initials

--- src/convert.ts
+++ src/convert.ts
@@ -1,12 +1,11 @@
-import { Response } from 'express';
+import {Response} from 'express';
 import converter from 'jsontoxml';
 
-import { Options } from '.';
+import {Options} from '.';
 
 export const convert = (response: Response) => async (data: any, options?: Options): Promise<void> => {
   const convertedData = converter(data, options);
 
   // response.header('Content-Type', 'application/xml');
-
   response.send(convertedData);
 };
