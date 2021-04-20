2bbaf588848490c04bf76f2b10179eaaff122122
Simon Skinner <simon@vult.uk>
2021-04-20 11:00:24 +0100

    0.0.7

--- src/Types/toXML.ts
+++ src/Types/toXML.ts
@@ -1,3 +1,3 @@
-import {Options} from './options';
+import { Options } from './options';
 
 export type toXML = (data: any, fileName: string, appendTimestamp?: boolean, options?: Options) => Promise<void>;
